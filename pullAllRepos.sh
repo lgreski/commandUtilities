7099d14fbb029800e555365af80e37465b461a8c
Len Greski <leonard.m@greski.com>
2017-04-16 11:01:13 -0400

    Update pullAllRepos.sh
    add comment, reference of original source

--- pullAllRepos.sh
+++ pullAllRepos.sh
@@ -3,5 +3,6 @@
 # run from directory where all git repositories are subdirectories
 #
 # note that you can change pull origin to pull, or pull origin master or another branchname
+# h/t Stackoverflow article -- http://stackoverflow.com/questions/3497123/run-git-pull-over-all-subdirectories
 
 find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull origin \;
